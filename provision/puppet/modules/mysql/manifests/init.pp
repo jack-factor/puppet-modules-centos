0e95b73689773b8698c62b483a044ec70b2ecb27
Jack Moreno <jack12972@gmail.com>
2015-07-24 14:43:44 -0500

    [feature/task07] add config user on mysql

--- provision/puppet/modules/mysql/manifests/init.pp
+++ provision/puppet/modules/mysql/manifests/init.pp
@@ -1,5 +1,7 @@
 class mysql
 {
+    $mysqlPassword = '123456'
+
     package { 'mysql-community-server':
         provider => 'rpm',
         ensure => installed,
@@ -12,6 +14,17 @@ class mysql
         ensure => running,
         require => Package['mysql-community-server'],
     }
+
+    exec { 'set-mysql-password':
+        onlyif => 'mysqladmin -uroot -pvagrant status',
+        command => 'mysqladmin -uroot -pvagrant password $mysqlPassword',
+        require => Service['mysqld'],
     
+    }
+
+    exec { 'enable-mysql':
+        command => 'chkconfig mysqld on',
+        require => Service['mysqld'],
+    }
 
 }
